aa032f23612eb711d374c7452492fa431cbf271f
sitowski.ms <sitowski.ms@pg.com>
2021-10-19 07:24:23 +0200

    Learning java - initial commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+// javac Hello.java
+
+public class Hello {
+
+    public static void main(String[] args) {
+        String message = "Hello Pluralsight!";
+        System.out.println(message);
+    }
+
+}
